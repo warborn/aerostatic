1e0b6969e5e285100ae97bba1327830f445e3b8c
Ivan Munguia <xxthecreatorxx_live@hotmail.com>
2016-06-02 07:40:05 +0000

    Redirect to panel when entering home page when login

--- app/controllers/pages_controller.php
+++ app/controllers/pages_controller.php
@@ -1,10 +1,21 @@
 <?php
 
 class PagesController extends ApplicationController {
+  public $before_action = ['redirect_if_logged_in'];
   protected $layout = 'public';
 
   public function index() {
 
+  }
+
+  public function redirect_if_logged_in() {
+    if(is_logged_in()){
+      if(is_admin_logged_in()) {
+        redirect_to('air-ballons');
+      } else {
+        redirect_to('travels');
+      }
+    }
   }
 }
 
